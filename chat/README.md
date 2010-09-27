4abddaa8e8a851b31a3e938a960e87c6fa848038
Peter Scott <pjscott@iastate.edu>
2010-09-26 22:05:56 -0500

    Added app.js to git. This is compiled from app.coffee, so this is kind of redundant, but it makes running the examples easier for people who check out the repo.

--- chat/README.md
+++ chat/README.md
@@ -1,14 +1,6 @@
 Chat Example
 ==========
 
-This shows how to write a web chat server with pubsubcore. The server itself is in `chatserver.js`, and the client is in the `webroot/` folder. The client JavaScript is actually written in ParenScript, but should be easy to follow even if you don't know that language. The main point is that it's really just subscribing to rooms and sending messages to channels, and opening a connection. Easy. The rest is just user interface stuff.
-
-To compile app.coffee so you can run the example, use this command:
-
-    $ coffee -c app.coffee
-    
-If you don't have CoffeeScript installed, you can get it with npm:
-
-    $ npm install coffee-script
+This shows how to write a web chat server with pubsubcore. The server itself is in `chatserver.js`, and the client is in the `webroot/` folder. The client JavaScript is actually written in ParenScript, but should be easy to follow even if you don't know that language. There's a precompiled version of the code in `app.js` if you just want to run the example. The main point is that it's really just subscribing to rooms and sending messages to channels, and opening a connection. Easy. The rest is just user interface stuff. For more details on how it works, see the main README file.
     
 This example requires [paperboy](http://github.com/felixge/node-paperboy/), for serving the static files. This should not be used in production, but it will do for a simple example.
---
+++ chat/webroot/app.js
@@ -0,0 +1,72 @@
+(function() {
+  var chat_handler, chatroom, get_user_handler, press_signin_button, pubsub, show_message, username;
+  username = null;
+  chatroom = null;
+  pubsub = new PubSubCore();
+  press_signin_button = function(e) {
+    if (e.keyCode === 13) {
+      return $("#signin-button").click();
+    }
+  };
+  show_message = function(name, text) {
+    return $('#chatwindow').append("<p><b>" + (name) + ":</b> " + (text) + "</p>");
+  };
+  $(function() {
+    $("#username").keypress(press_signin_button);
+    $("#chatroom").keypress(press_signin_button);
+    $("#signin-button").click(function() {
+      username = $("#username").val();
+      chatroom = $("#chatroom").val().replace(/\//g, '');
+      if (username.length === 0 || chatroom.length === 0) {
+        return alert("You must enter a username and chatroom");
+      } else {
+        pubsub.join_room(username, chatroom);
+        pubsub.add_handler('/command/list', get_user_handler);
+        pubsub.add_handler(chatroom, chat_handler);
+        $("#signin").hide();
+        return $("#chat").show();
+      }
+    });
+    return $("#chatline").keypress(function(e) {
+      var text;
+      if (e.keyCode === 13) {
+        text = $("#chatline").val();
+        if (text === '/list') {
+          pubsub.send('/command/list', {
+            room: chatroom
+          });
+        } else {
+          pubsub.send(chatroom, {
+            name: username,
+            text: text
+          });
+        }
+        return $("#chatline").val('');
+      }
+    });
+  });
+  chat_handler = function(msg) {
+    return show_message(msg.data.name, msg.data.text);
+  };
+  get_user_handler = function(msg) {
+    var users_str;
+    users_str = msg.users.join(', ');
+    return $('#chatwindow').append("<p><i><b>Users:</b> " + (users_str) + "</i></p>");
+  };
+  pubsub.onannounce = function(msg) {
+    return $('#chatwindow').append("<p><i>" + (msg.name) + " " + (msg.action) + "</i></p>");
+  };
+  pubsub.onconnect = function() {
+    $("#log").append("<p>Connected to server</p>");
+    return (typeof username !== "undefined" && username !== null) ? pubsub.join_room(username, chatroom) : null;
+  };
+  pubsub.ondisconnect = function() {
+    return $("#log").append("<p>Server connection lost</p>");
+  };
+  pubsub.onmessage = function(msg) {
+    var json_msg;
+    json_msg = JSON.stringify(msg);
+    return $("#log").append("<p>Message: " + (json_msg) + "</p>");
+  };
+  pubsub.connect();
+})();
