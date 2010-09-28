e1ab0ac46a4e7953ab6ac004090d1f3d9d17b29c
Peter Scott <pjscott@iastate.edu>
2010-09-27 23:00:15 -0500

    Chat server now denies messages from users not in the room they send to.

--- chat/chatserver.js
+++ chat/chatserver.js
@@ -24,14 +24,16 @@ pubsub.add_handler('/command/list', function(client, msg) {
 // Requests to any other channel are treated as chat messages.
 pubsub.add_handler(/.*/, function(client, msg) {
     console.log("MSG:", sys.inspect(msg));
-    // Treat the channel name as the room name.
-    pubsub.broadcast_room(msg.channel, {
-        room: msg.channel,
-        data: {
-            name: msg.data.name,
-            text: msg.data.text
-        }
-    });
+    // Treat the channel name as the room name. Turn away messages
+    // from clients who aren't in the room.
+    if (pubsub.client_in_room(msg.channel, client))
+	pubsub.broadcast_room(msg.channel, {
+            room: msg.channel,
+            data: {
+		name: msg.data.name,
+		text: msg.data.text
+            }
+	});
 });
 
 // Start the server
